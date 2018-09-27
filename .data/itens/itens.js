d5b8e98986cf30d6068e11f33e54ba49849a3a87
Giovani Mazuchelli Del Col <giovanimdelcol@gmail.com>
2018-09-27 11:05:28 -0300

    menu commit

---
+++ .data/itens/itens.js
@@ -0,0 +1,23 @@
+//hardcodede itens emulating some database records
+const itens = {
+   available: [
+        {
+            'productName' : 'Burguer',
+            'Price' : 10.5,
+        },
+        {
+            'productName' : 'Pizza',
+            'Price' : 4,
+        },
+        {
+            'productName' : 'Orange Juice',
+            'Price' : 3,
+        },
+        {
+            'productName' : 'Soda',
+            'Price' : 5,
+        }
+    ]
+};
+
+module.exports = itens;
